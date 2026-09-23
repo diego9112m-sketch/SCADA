87a444add4fa0befeeced90316bca9e7a10939fd
diego9112m-sketch <diego9112.m@gmail.com>
2026-09-23 14:56:32 -0500

    Add files via upload

---
+++ unifilar_config.js
@@ -0,0 +1,140 @@
+/**
+ * Configuraciones predeterminadas de los diagramas unifilares.
+ * Reemplaza este archivo con el descargado desde la interfaz.
+ */
+window.UNIFILAR_CONFIG = {
+    "inst-1": {
+        "bounds": {
+            "minX": 0,
+            "maxX": 19,
+            "minY": 1,
+            "maxY": 14
+        },
+        "state": {
+            "3,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "4,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "5,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "6,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "7,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "8,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "9,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "10,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "11,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "12,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "13,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "14,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "15,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "16,4": {
+                "type": "bus",
+                "rotation": 0
+            },
+            "9,6": {
+                "type": "meter",
+                "meterId": "m-02"
+            },
+            "9,7": {
+                "type": "t-join",
+                "rotation": 180
+            },
+            "14,9": {
+                "type": "load",
+                "rotation": 0
+            },
+            "14,8": {
+                "type": "line-v",
+                "rotation": 0
+            },
+            "5,8": {
+                "type": "trafo",
+                "rotation": 0
+            },
+            "5,9": {
+                "type": "meter",
+                "meterId": "m-01"
+            },
+            "9,5": {
+                "type": "trafo",
+                "rotation": 0
+            },
+            "10,7": {
+                "type": "line-h",
+                "rotation": 0
+            },
+            "11,7": {
+                "type": "line-h",
+                "rotation": 0
+            },
+            "12,7": {
+                "type": "line-h",
+                "rotation": 0
+            },
+            "13,7": {
+                "type": "line-h",
+                "rotation": 0
+            },
+            "8,7": {
+                "type": "line-h",
+                "rotation": 0
+            },
+            "6,7": {
+                "type": "line-h",
+                "rotation": 0
+            },
+            "7,7": {
+                "type": "line-h",
+                "rotation": 0
+            },
+            "5,7": {
+                "type": "corner",
+                "rotation": 0
+            },
+            "14,7": {
+                "type": "corner",
+                "rotation": 90
+            },
+            "5,10": {
+                "type": "gen",
+                "rotation": 0
+            }
+        }
+    }
+};
